a347eb82e90c5250d3729621397c9da161c95cfc
kjmczk <vollgong@gmail.com>
2020-10-13 14:02:25 +0900

    Use `screenOptions` prop instead of `headerMode`

--- navigation/AuthStack.tsx
+++ navigation/AuthStack.tsx
@@ -11,7 +11,9 @@ const Stack = createStackNavigator<AuthStackParamList>();
 
 const AuthStack: React.FC = () => {
   return (
-    <Stack.Navigator initialRouteName="Welcome" headerMode="none">
+    <Stack.Navigator
+      initialRouteName="Welcome"
+      screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
